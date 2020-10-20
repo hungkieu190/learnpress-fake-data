6a0e23baa10a52e6cf6115c02697b71dc4eeea32
hungkieu190 <hungkieu190@gmail.com>
2020-10-20 17:17:30 +0700

    Create learnpress-fake-data.php

---
+++ learnpress-fake-data.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: LearnPress - Fake Data
+Plugin URI: http://thimpress.com/learnpress
+Description: Create fake data Learnpress.
+Author: ThimPress
+Version: 1.0.0
+Author URI: http://thimpress.com
+Tags: learnpress, lms, add-on, fake-data
+Text Domain: learnpress-fake-data
+Domain Path: /languages/
+*/
